2d3c97721ce01fe8984e951eb6ca2bee157e5251
PAITSUN VIKTORIIA <paitsunviktoriia@gmail.com>
2025-02-10 15:53:24 +0100

    do task-1 and task-2

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+"use strikt";
+const categoriesList = document.querySelector("#categories");
+const categories = categoriesList.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) => {
+  const title = category.querySelector("h2").textContent;
+  const itemCount = category.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${itemCount}`);
+});
