145bf7fa3d20366d474a92cb4183c1da8fd827ba
PAITSUN VIKTORIIA <paitsunviktoriia@gmail.com>
2025-02-12 14:54:12 +0100

    do task-4 and task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777216)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.body;
+
+changeColorButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
